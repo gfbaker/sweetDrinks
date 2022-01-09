bd96de7fc174581ad18125554efd7fc3ebeabc28
maracprado <maracprado@gmail.com>
2022-01-09 09:58:50 -0300

    Arregla isAdminMiddleware segun nuevo formato de usuarioLogueado

--- middlewares/isAdminMiddleware.js
+++ middlewares/isAdminMiddleware.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const isAdminMiddleware= (req,res,next) => {
 
-    if (req.session.usuarioLogueado.privilegios != "admin"){
+    if (req.session.usuarioLogueado.usersAuthData.admin != true){
         res.render (path.join(__dirname,"../src/views/not-found"));
     }else{
         next();
